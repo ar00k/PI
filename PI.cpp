b201aacd0b972459a36be92eb464701fb705937a
ar00k <arkadiusz.ryczek@onet.pl>
2024-12-28 18:37:54 +0100

    calculating partial PI

--- PI.cpp
+++ PI.cpp
@@ -18,5 +18,38 @@ double PI::computePI() {
     // Tablica na wyniki częściowe obliczone przez każdy wątek
     std::vector<double> partialResults(numThreads, 0.0);
     std::vector<std::thread> threads;
+
+    // Rozpoczynamy wątki
+    for (int i = 0; i < numThreads; ++i) {
+        threads.emplace_back(&PI::calculatePartialPI, this, i, std::ref(partialResults));
+    }
+
+    // Czekamy na zakończenie pracy wszystkich wątków
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    // Sumowanie wyników z wszystkich wątków
+    double totalResult = 0.0;
+    for (const auto& partial : partialResults) {
+        totalResult += partial;
+    }
+
+    return totalResult;
 }
 
+// Funkcja obliczająca część całki przypisaną do danego wątku
+void PI::calculatePartialPI(int threadId, std::vector<double>& partialResults) {
+    double localResult = 0.0;
+    long long start = threadId * (numDivisions / numThreads);
+    long long end = (threadId + 1) * (numDivisions / numThreads);
+
+    // Obliczamy część całki dla tego wątku
+    for (long long i = start; i < end; ++i) {
+        double x = (i + 0.5) / numDivisions;  // Środek podprzedziału
+        localResult += 4.0 / (1.0 + x * x);   // Funkcja dla całki
+    }
+
+    // Zapisujemy wynik w odpowiedniej pozycji w tablicy
+    partialResults[threadId] = localResult / numDivisions;
+}
