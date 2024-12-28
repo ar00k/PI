a0531889ee246d12a350ef7abbf8deda62fd3fd9
ar00k <arkadiusz.ryczek@onet.pl>
2024-12-28 18:35:01 +0100

    Compute PI fix

--- PI.cpp
+++ PI.cpp
@@ -12,3 +12,11 @@
 PI::PI(long long numDivisions, int numThreads)
     : numDivisions(numDivisions), numThreads(numThreads) {
 }
+
+// Metoda obliczająca wartość liczby PI
+double PI::computePI() {
+    // Tablica na wyniki częściowe obliczone przez każdy wątek
+    std::vector<double> partialResults(numThreads, 0.0);
+    std::vector<std::thread> threads;
+}
+
